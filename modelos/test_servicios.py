f1584f2fad7ff984f2486746b4f3937fc74d10ed
Paula Andrea Gonzalez H <paulaandrea0316@yahoo.com.mx>
2026-04-23 20:31:04 -0500

    Eliminacion archivo
    se elimina el archivo test_servicios.py que se hizo para porbar el funcionamiento del codigo

--- modelos/test_servicios.py
+++
@@ -1,9 +0,0 @@
-from modelos.servicios_derivados import ReservaSala, AlquilerEquipo, AsesoriaEspecializada
-
-sala = ReservaSala("Sala", 50000)
-equipo = AlquilerEquipo("Laptop", 30000)
-asesoria = AsesoriaEspecializada("Consultoría", 80000)
-
-print("Sala:", sala.calcular_costo(2))
-print("Equipo:", equipo.calcular_costo(2))
-print("Asesoría:", asesoria.calcular_costo(2))
